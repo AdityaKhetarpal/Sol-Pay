055cb05a5ce6a3398aa744b46a59021b5ca8ff36
Aditya Khetarpal <80028073+AdityaKhetarpal@users.noreply.github.com>
2022-12-31 10:06:49 +0530

    Update IpfsDownload.js

--- IpfsDownload.js
+++ IpfsDownload.js
@@ -9,7 +9,8 @@ const IPFSDownload = ({ hash, filename }) => {
     <div>
       {file ? (
         <div className="download-component">
-          <a className="download-button" href={file} download={filename}>Download</a>
+          <a className="download-button" href={"https://cloudflare-ipfs.com/ipfs/Qma1FmHULD4Wz7gaDFNxKziGBxmkkjQmpMu6bbFBJxmLRW/Blender.png"} download={filename}>Download</a>
+          <a className="download-button" href={"https://cloudflare-ipfs.com/ipfs/Qma1FmHULD4Wz7gaDFNxKziGBxmkkjQmpMu6bbFBJxmLRW/Abstimmung.png"} download={filename}>Download</a>
         </div>
       ) : (
         <p>Downloading file...</p>
